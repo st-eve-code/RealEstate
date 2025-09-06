807706c5d27d8ee9ab4a6bffebb165c0b2340cd2
st-eve-code <stevecaleb37@gmail.com>
2025-09-06 13:39:23 +0100

    signup changes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -45,8 +45,8 @@ function Signup() {
   };
 
   return (
-    <section className='flex justify-center items-center  bg-gray-50'>
-      <div className='max-w-[28rem] shadow-lg shadow-gray-300 mx-auto p-8 rounded-xl'>
+    <section className='flex justify-center items-center min-h-screen lg:h-[40rem] p-[0.5rem] bg-gray-50'>
+      <div className='max-w-[28rem] shadow-lg shadow-gray-300 mx-auto px-8  rounded-xl'>
         <img src={logo} alt="Rentspot logo" className='w-[11rem] py-4 mx-auto' />
         <p className='font-Nunito font-medium text-sm text-gray-500 text-center'>
           Welcome to Rentspot where every rental property has a calling. Fill in the form to get started.
