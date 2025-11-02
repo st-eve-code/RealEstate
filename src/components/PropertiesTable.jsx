de0a0d64dfc3935d3e736e91fffbd8cf980dc1fe
st-eve-code <stevecaleb37@gmail.com>
2025-11-02 20:02:55 +0000

    dashboard

--- src/components/PropertiesTable.jsx
+++ src/components/PropertiesTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../Lib/supabaseClient';
 import { 
   Search, 
   Filter, 
