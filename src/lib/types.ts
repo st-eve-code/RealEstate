4f7653ec6e17e90b8e2967ba069ab8de610a2302
IamSupreme <danybeloved@gmail.com>
2025-12-19 01:47:54 +0100

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -9,8 +9,11 @@ export type VerificationStatus = "pending" | "verified" | "rejected"
 
 export type SubscriptionStatus = "active" | "expired" | "pending"
 
-export type SubscriptionPlan = "daily" | "monthly" | "yearly"
+export type SubscriptionPlanLabel = "daily" | "monthly" | "yearly"
 
+/**
+ * Payment Method for Momo
+ */
 export interface PaymentMOMO {
     id: string,
     uid: string,
@@ -21,6 +24,9 @@ export interface PaymentMOMO {
     paymentType: 'momo'
 }
 
+/**
+ * Payment Method for Card
+ */
 export interface PaymentCard {
     id: string,
     uid: string,
@@ -33,19 +39,26 @@ export interface PaymentCard {
     paymentType: 'card'
 }
 
+/**
+ * Payment Transaction but not used again for this project
+ * @deprecated no need as only subscription payments are needed for this project
+ */
 export interface Payment{
     id: string,
     name: string,
-    plan: SubscriptionPlan,
+    plan: SubscriptionPlanLabel,
     amount: number,
     type: 'payment'
 }
 
+/**
+ * Subscription Transaction, containing subscription details like expiry date, subscription plan details and payment details
+ */
 export interface Subscription {
     id: string,
     name: string,
     description: string,
-    plan: SubscriptionPlan,
+    plan: SubscriptionPlanLabel,
     amount: number,
     accType?: "tenant"|"landlord",
     type: 'subscription',
@@ -58,6 +71,10 @@ export interface Subscription {
     }
 }
 
+/**
+ * Transaction, containing subscription details and payment details
+ * In firebase, it is stored in the users/uid/Transactions collection
+ */
 export interface Transaction {
     id: string,
     uid: string,
@@ -72,7 +89,9 @@ export interface Transaction {
 }
 
 
-
+/**
+ * landlord details like documents among other things
+ */
 export interface LandLord {
     licenseNumber: string,
     documents: string[]
@@ -93,7 +112,7 @@ export interface AdminRole{
 }
 
 /**
- * Also includes Transactions collection, Favorites collection, Payment Methods collection
+ * In firebase also includes Transactions subcollection, Favorites subcollection, Payment Methods subcollection
  */
 export interface User {
     uid: string,
@@ -119,10 +138,27 @@ export interface User {
     loadedUnits: Unit[],
 
     subscription?: Transaction | Omit<Transaction, "uid">, // subscription made IDs
-    autoRenewal?: boolean
+    autoRenewal?: boolean,
+    fcmToken?: string,
+    deviceInfo?: {
+        currentDeviceToken?: string,
+        deviceType?: string,
+        deviceModel?: string,
+        deviceOS?: string,
+        deviceOSVersion?: string,
+        deviceOSBuild?: string,
+        deviceOSBuildVersion?: string,
+        deviceOSBuildVersionCode?: string,
+        deviceOSBuildVersionName?: string,
+    }
 
 }
 
+/**
+ * Subscription Plan, containing plan details like name, price, duration and features. 
+ * This is used to display the plan details to the user for them to subscribe to and to
+ * in firebase, it is stored in the plans collection
+ */
 export interface Plan {
     id: number;
     name: string;
@@ -139,7 +175,8 @@ export interface GeoLocate{
 
 /**
  * Unit properties
- * @description on firebase have collections: likes, views,
+ * @description on firebase have collections: likes, views, reviews
+ * in firebase, it is stored in the units collection
  */
 export interface Unit {
     id: string,
@@ -165,8 +202,26 @@ export interface Unit {
     // reviews: {user: string, star: number, comment?: string}[] // read 
     features?: string[] // Wifi, water
     tags: string[] // apartment, street, in-door toilet (in general additional search params)
+    views: number, // number of times the unit has been viewed
 }
 
+
+/**
+ * Unit View, containing view details like user, unit and timestamp
+ * to be able to track unit views per time range for analytics
+ * in firebase, stored in units/id/UnitViews subcollection
+ */
+export interface UnitView {
+    id: string, // this id
+    uid: string, // user that viewed the unit
+    unit: string, // unit that was viewed
+    createdAt: Timestamp, 
+}
+
+/**
+ * Unit Review, containing review details like rating, comment, author and timestamp
+ * in firebase, it is stored in the units/id/reviews subcollection
+ */
 export interface UnitReview{
     id: string, // this id
     uid: string, // user that sent review
@@ -179,6 +234,10 @@ export interface UnitReview{
     updatedAt: Timestamp,
 }
 
+/**
+ * To be touched later
+ * @todo unsure of what this is for
+ */
 export interface Property {
     id: number;
     buildingId: number; // FK to building.id
@@ -187,6 +246,10 @@ export interface Property {
     rentAmount: number;
 }
 
+/**
+ * To be touched later
+ * @todo unsure of what this is for
+ */
 export interface UserRent {
     buildingId: string,
     roomId: string,
@@ -196,6 +259,9 @@ export interface UserRent {
 }
 
 
+/**
+ * 
+ */
 export interface SortConfig {
     field: string;
     direction: OrderByDirection; // 'asc' or 'desc'
